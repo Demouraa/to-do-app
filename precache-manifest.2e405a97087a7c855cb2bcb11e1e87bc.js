71dd47dc06c4a6d4abc6636afeee9f9604bf69e8
Matheus Gomes de Moura <matheusdemoura@protonmail.com>
2020-01-10 05:05:21 -0300

    Updates

---
+++ precache-manifest.2e405a97087a7c855cb2bcb11e1e87bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30fd64a92ac6203747e1f8daf41ae08",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "a6a518ace3ec3d06e17f",
+    "url": "/to-do-app/static/css/main.4858be47.chunk.css"
+  },
+  {
+    "revision": "774dc6829b9cd1365170",
+    "url": "/to-do-app/static/js/2.cbbcb3d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-app/static/js/2.cbbcb3d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6a518ace3ec3d06e17f",
+    "url": "/to-do-app/static/js/main.e9f8bcc1.chunk.js"
+  },
+  {
+    "revision": "0c31dae972f17124d9fa",
+    "url": "/to-do-app/static/js/runtime-main.9a3ec0f1.js"
+  }
+]);
